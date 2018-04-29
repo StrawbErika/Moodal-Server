feb6aec275ce5d854b56f20f3b3de1bae416ded9
Kobe Jee De Luna <kbdeluna@up.edu.ph>
2018-04-29 23:16:01 +0800

    Add entities folder contents to the folder structure

--- build/main.js
+++ build/main.js
@@ -69,29 +69,61 @@ module.exports = /******/ (function(modules) {
   /******/
   /******/ /******/ __webpack_require__.p = '/'; // Load entry module and return exports
   /******/
-  /******/ /******/ return __webpack_require__((__webpack_require__.s = 0));
+  /******/ /******/ return __webpack_require__((__webpack_require__.s = 1));
   /******/
 })(
   /************************************************************************/
   /******/ [
     /* 0 */
-    /***/ function(module, exports, __webpack_require__) {
-      module.exports = __webpack_require__(1);
+    /***/ function(module, exports) {
+      module.exports = require('express');
 
       /***/
     },
     /* 1 */
+    /***/ function(module, exports, __webpack_require__) {
+      module.exports = __webpack_require__(2);
+
+      /***/
+    },
+    /* 2 */
     /***/ function(module, __webpack_exports__, __webpack_require__) {
       'use strict';
       Object.defineProperty(__webpack_exports__, '__esModule', { value: true });
       /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_express__ = __webpack_require__(
-        2
+        0
       );
       /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_express___default = __webpack_require__.n(
         __WEBPACK_IMPORTED_MODULE_0_express__
       );
+      /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_body_parser__ = __webpack_require__(
+        3
+      );
+      /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_body_parser___default = __webpack_require__.n(
+        __WEBPACK_IMPORTED_MODULE_1_body_parser__
+      );
+      /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_morgan__ = __webpack_require__(
+        4
+      );
+      /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2_morgan___default = __webpack_require__.n(
+        __WEBPACK_IMPORTED_MODULE_2_morgan__
+      );
+      /* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__router__ = __webpack_require__(
+        5
+      );
 
       const app = __WEBPACK_IMPORTED_MODULE_0_express___default()();
+
+      // Middlewares
+      app.use(__WEBPACK_IMPORTED_MODULE_1_body_parser___default.a.json());
+      app.use(
+        __WEBPACK_IMPORTED_MODULE_1_body_parser___default.a.urlencoded({
+          extended: true
+        })
+      );
+      app.use(__WEBPACK_IMPORTED_MODULE_2_morgan___default()('dev'));
+
+      app.use(__WEBPACK_IMPORTED_MODULE_3__router__['a' /* default */]);
 
       app.listen(process.env.port || 3002, () => {
         console.log('Listening to port 3002');
@@ -99,9 +131,31 @@ module.exports = /******/ (function(modules) {
 
       /***/
     },
-    /* 2 */
+    /* 3 */
     /***/ function(module, exports) {
-      module.exports = require('express');
+      module.exports = require('body-parser');
+
+      /***/
+    },
+    /* 4 */
+    /***/ function(module, exports) {
+      module.exports = require('morgan');
+
+      /***/
+    },
+    /* 5 */
+    /***/ function(module, __webpack_exports__, __webpack_require__) {
+      'use strict';
+      /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_express__ = __webpack_require__(
+        0
+      );
+      /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_express___default = __webpack_require__.n(
+        __WEBPACK_IMPORTED_MODULE_0_express__
+      );
+
+      const router = Object(__WEBPACK_IMPORTED_MODULE_0_express__['Router'])();
+
+      /* harmony default export */ __webpack_exports__['a'] = router;
 
       /***/
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import logger from 'morgan';
+
+import router from './router';
 
 const app = express();
+
+// Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(logger('dev'));
+
+app.use(router);
 
 app.listen(process.env.port || 3002, () => {
   console.log('Listening to port 3002');
