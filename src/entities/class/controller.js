75beac37932aee07ab16b23300ba4b53df8da0c4
Kobe Jee De Luna <kbdeluna@up.edu.ph>
2018-04-30 08:17:36 +0800

    Add class controller

--- src/entities/class/controller.js
+++ src/entities/class/controller.js
@@ -1,1 +1,47 @@
 import { Class } from '../../database';
+
+export const createClass = _class => {
+  return new Promise((resolve, reject) => {
+    const newClass = new Class({ ..._class });
+
+    newClass.save((err, _class) => {
+      if (err) {
+        console.log(err);
+        return reject(500);
+      }
+
+      return resolve(_class);
+    });
+  });
+};
+
+export const deleteClass = _id => {
+  return new Promise((resolve, reject) => {
+    Class.findOneAndRemove({ _id }, err => {
+      if (err) {
+        console.log(err);
+        return reject(500);
+      }
+      return resolve();
+    });
+  });
+};
+
+export const editClass = (_id, { title, section, posts, students }) => {
+  return new Promise((resolve, reject) => {
+    Class.findById(_id, (err, _class) => {
+      if (err) {
+        console.log(err);
+        return reject(500);
+      }
+      if (!_class) {
+        return reject(404);
+      }
+
+      _class.title = title;
+      _class.section = section;
+      _class.posts = posts;
+      _class.students = students;
+    });
+  });
+};
