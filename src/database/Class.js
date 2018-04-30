7755b9dab612f7c79cdd5da6e4d8d79b398b5b92
Kobe Jee De Luna <kbdeluna@up.edu.ph>
2018-04-30 09:04:48 +0800

    Edit schema

--- src/database/Class.js
+++ src/database/Class.js
@@ -4,7 +4,9 @@ const ClassSchema = new Schema({
   title: { type: String, required: true },
   section: { type: String, required: true },
   posts: { type: Array, required: true },
-  students: { type: Array, required: true }
+  students: { type: Array, required: true },
+  canPost: { type: Boolean, required: true },
+  canComment: { type: Boolean, required: true }
 });
 
 export const Class = mongoose.model('Class', ClassSchema);
--- src/database/User.js
+++ src/database/User.js
@@ -4,7 +4,8 @@ const UserSchema = new Schema({
   email: { type: String, required: true },
   name: { type: String, required: true },
   password: { type: String, required: true },
-  userType: { type: String, required: true }
+  userType: { type: String, required: true },
+  classes: { type: Array, required: true }
 });
 
 export const User = mongoose.model('User', UserSchema);
