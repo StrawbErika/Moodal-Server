9f7f1e55beff0b133fcf10fb1b81035eaf0c6b3d
JomaUrbano <muurbano@up.edu.ph>
2018-05-01 02:50:51 +0800

    Added Router + Controller for Messages, Update database

--- src/entities/message/controller.js
+++ src/entities/message/controller.js
@@ -1,1 +1,78 @@
-import { Message } from '../../database';
+import { Message } from '../../database/Message';
+
+export const viewAllMessage = () => {
+  return new Promise((resolve, reject) => {
+    Message.find((err, messagees) => {
+      if (err) {
+        console.log(err);
+        return reject(500);
+      }
+      return resolve(messagees);
+    });
+  });
+};
+
+export const viewMessageByID = _id => {
+  return new Promise((resolve, reject) => {
+    Message.findById(_id, (err, message) => {
+      if (err) {
+        console.log(err);
+        return reject(500);
+      }
+      return resolve(message);
+    });
+  });
+};
+
+export const createMessage = message => {
+  return new Promise((resolve, reject) => {
+    const newMessage = new Message(message);
+
+    newMessage.save((err, message) => {
+      if (err) {
+        console.log(err);
+        return reject(500);
+      }
+
+      return resolve(message);
+    });
+  });
+};
+
+export const deleteMessage = _id => {
+  return new Promise((resolve, reject) => {
+    Message.findOneAndRemove({ _id }, err => {
+      if (err) {
+        console.log(err);
+        return reject(500);
+      }
+      return resolve();
+    });
+  });
+};
+
+export const editMessage = (_id, { sender, recipient, content, timestamp }) => {
+  return new Promise((resolve, reject) => {
+    Message.findById(_id, (err, message) => {
+      if (err) {
+        console.log(err);
+        return reject(500);
+      }
+      if (!message) {
+        return reject(404);
+      }
+      message.sender = sender;
+      message.recipient = recipient;
+      message.content = content;
+      message.timestamp = timestamp;
+
+      message.save((err, newMessage) => {
+        if (err) {
+          console.log(err);
+          return reject(500);
+        }
+        return resolve(newMessage);
+      });
+    });
+  });
+};
--- src/entities/message/router.js
+++ src/entities/message/router.js
@@ -1,4 +1,114 @@
 import { Router } from 'express';
 import * as Ctrl from './controller';
 
+const router = Router();
+
+//Gets all message
+router.get('/api/message', async (req, res) => {
+  try {
+    const messages = await Ctrl.viewAllMessage(req.body);
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully fetched all messages',
+      data: messages
+    });
+  } catch (status) {
+    let message = '';
+    switch (status) {
+      case 404:
+        message = 'Message not found';
+        break;
+      case 500:
+        message = 'Internal server error';
+        break;
+    }
+    res.status(status).json({ status, message });
+  }
+});
+
+//Gets message by id
+router.get('/api/message/:_id', async (req, res) => {
+  try {
+    const messages = await Ctrl.viewMessageByID(req.params);
+    res.status(200).json({
+      status: 200,
+      message: 'Successfully fetched message',
+      data: messages
+    });
+  } catch (status) {
+    let message = '';
+    switch (status) {
+      case 404:
+        message = 'Message not found';
+        break;
+      case 500:
+        message = 'Internal server error';
+        break;
+    }
+    res.status(status).json({ status, message });
+  }
+});
+
+//Adds a message
+router.post('/api/message', async (req, res) => {
+  if (
+    req.body.sender &&
+    req.body.recipient &&
+    req.body.content &&
+    req.body.timestamp
+  ) {
+    try {
+      await Ctrl.createMessage(req.body);
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully created message'
+      });
+    } catch (status) {
+      res.status(500).json({ status: 500, message: 'Internal server error' });
+    }
+  } else {
+    res.status(400).json({ status: 400, message: 'Bad request' });
+  }
+});
+
+//Deletes a message
+router.delete('/api/message/:_id', async (req, res) => {
+  if (req.params._id) {
+    try {
+      await Ctrl.deleteMessage(req.params);
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully removed message'
+      });
+    } catch (status) {
+      res.status(500).json({ status: 500, message: 'Internal server error' });
+    }
+  } else {
+    res.status(400).json({ status: 400, message: 'Bad request' });
+  }
+});
+
+//Edits/Updates a message
+router.put('/api/message/:_id', async (req, res) => {
+  if (
+    req.params._id &&
+    req.body.sender &&
+    req.body.recipient &&
+    req.body.content &&
+    req.body.timestamp
+  ) {
+    try {
+      await Ctrl.editMessage(req.params, req.body);
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully edited the message'
+      });
+    } catch (status) {
+      res.status(500).json({ status: 500, message: 'Internal server error' });
+    }
+  } else {
+    res.status(400).json({ status: 400, message: 'Bad request' });
+  }
+});
+
 export default router;
